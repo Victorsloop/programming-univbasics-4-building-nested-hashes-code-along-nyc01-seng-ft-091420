792dc85818d0da2914d766899693f02190a075ef
Victor Torres <torres.victor136@gmail.com>
2020-08-26 03:04:04 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,16 +1,7 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch => {:name => "Lord Montague", :age -> "50"},
-       :matriarch => {},
-       :hero => {},
-       :hero_friend => []
-     },
-     :capulet => {
-       :patriarch => {},
-       :matriarch =>{},:
-       :heroine => {},
-       :heroine_friend => []
-     }
+    :montague => {},
+     :capulet => {}
+   }
    }
 end
